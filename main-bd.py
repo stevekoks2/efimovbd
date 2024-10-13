df670b0b59d6b01ecd78721cecd2d43ff701d432
SteveKoks <stevekoks@gmail.com>
2024-10-13 03:40:17 +0300

    mentam pizda

---
+++ main-bd.py
@@ -0,0 +1,3 @@
+import manipulator
+
+manipulator.create_table("porno", "id", "video", "genre", 'duration')
---
+++ manipulator.py
@@ -0,0 +1,36 @@
+print("░▒▓████████▓▒░▒▓████████▓▒░▒▓█▓▒░▒▓██████████████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓███████▓▒░  ")
+print("░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ")
+print("░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ")
+print("░▒▓██████▓▒░ ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒▒▓█▓▒░░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░ ")
+print("░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒")
+print("░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░")
+print("░▒▓████████▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░   ░▒▓██▓▒░  ░▒▓███████▓▒░░▒▓███████▓▒░ ")                                                                                                     
+
+def create_table(name, *poles):
+    open(f'{name}.efimov', 'w')
+    with open(f'{name}.efimov', 'r') as file:
+        lines = file.readlines()
+    lines.insert(1 ,f'NAME:{name}\n')
+    with open(f'{name}.efimov', 'w') as file:
+        file.writelines(lines)
+    i = 3
+    for pole in poles:
+        with open(f'{name}.efimov', 'r') as file:
+            lines = file.readlines()
+        lines.insert(i ,f'{pole}:\n')
+        with open(f'{name}.efimov', 'w') as file:
+            file.writelines(lines)
+        i+=1
+    print(f"База {name} создана!")
+
+def insert_data(basa ,pole, data):
+    with open(f'{basa}.efimov', 'r') as file:
+        lines = file.readlines()
+    for i, line in enumerate(lines):
+        if line.startswith(f"{pole}:"):
+            lines[i] = line.rstrip() + " " + data + "," + '\n'
+            break
+    
+    with open(f'{basa}.efimov', 'w') as file:
+        file.writelines(lines)
+    print(f"Информация в базе {basa} в поле {pole} добавлена!")
